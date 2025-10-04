1a32b264d17a17a2c722926728e0c0ad7579812b
CHIMERA <hex.penrose.io@gmail.com>
2025-10-04 16:30:13 -0400

    removed unnecessary imports

--- core.py
+++ core.py
@@ -1,9 +1,4 @@
-import bs4.element
-from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 import time
 

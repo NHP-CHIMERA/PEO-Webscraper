96cbcafc95bff8d460137d1c283cd64ea5f68df2
CHIMERA <hex.penrose.io@gmail.com>
2025-10-04 16:27:00 -0400

    main file that runs all operations

---
+++ core.py
@@ -0,0 +1,44 @@
+import bs4.element
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+import time
+
+from data_handler import DataHandler
+from searcher import WebSearcher
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    sel_driver = webdriver.Firefox()
+    try:
+        sel_driver.get("https://www.peogrenada.org/ElectorialListing/Method2")
+
+        searcher = WebSearcher(sel_driver)
+
+        start = time.time()
+
+        searcher.main_search()
+        search_end = time.time()
+        print(f'Search Time: {search_end - start:.2f} seconds')
+
+        csv_start = time.time()
+        csv_conv = DataHandler(searcher.get_data())
+        csv_conv.to_csv()
+        csv_end = time.time()
+        print(f'Conversion Time: {csv_end - csv_start:.2f} seconds')
+
+        print(f'Total Time: {csv_end - start:.2f} seconds')
+
+
+    except TimeoutException:
+        print("Timed out waiting for results")
+    finally:
+        sel_driver.quit()
